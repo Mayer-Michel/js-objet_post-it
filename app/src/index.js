f853fe587c5556351eb6e3d130d34bc328e33b0c
Mayer MICHEL <mayer.mohib@gmail.com>
2025-02-18 19:02:50 +0100

    frontend start

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,6 @@
 // avec webpack, la précision ".js" n'est pas obligatoire
 // "app" n'est pas entre accolades, car il est en "export default"
 // Les accolades sont utilisées pour les "export" tout-court, pour gérer le cas où il y en a plusieurs 
-import app from './AppPostIts/App';
+import app from './AppCheck/App';
 
 app.start();
